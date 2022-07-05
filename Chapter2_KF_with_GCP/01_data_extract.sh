77b8c269137f068278bbe10e4823266b6cda0e31
Gary Hutson <gary.hutson.uk@hotmail.co.uk>
2022-07-05 11:06:29 +0000

    Added working data extract script

--- Chapter2_KF_with_GCP/01_data_extract.sh
+++ Chapter2_KF_with_GCP/01_data_extract.sh
@@ -1,12 +1,20 @@
-gcloud init
+sudo gcloud init
+#sudo gcloud auth login
+sudo gcloud auth configure-docker
 # Navigate to the relevant directory
 cd pipeline/1_dataextraction
 ls
-PROJECT_ID=$(gcloud config get-value core/project)
+#PROJECT_ID=$(gcloud config get-value core/project)
+PROJECT_ID=crisp-gpu-training
 CONTAINER_REG_IMAGE_NAME=breast_cancer/1_loadingdata
 IMAGE_VERSION=v1
 IMAGE_NAME=gcr.io/$PROJECT_ID/$CONTAINER_REG_IMAGE_NAME
 
 # Build the image
 sudo docker build -t $IMAGE_NAME:$IMAGE_VERSION .
+echo '[INFO] listing the container images'
+echo '======================================================'
 sudo docker images
+
+# Push the docker image to the container registry 
+sudo docker push $IMAGE_NAME:$IMAGE_VERSION
