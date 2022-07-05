daa1cc1224512a17586332178454a06bdb7c328f
Gary Hutson <gary.hutson.uk@hotmail.co.uk>
2022-07-05 10:33:53 +0000

    Updated bash

--- Chapter2_KF_with_GCP/01_data_extract.sh
+++ Chapter2_KF_with_GCP/01_data_extract.sh
@@ -3,4 +3,10 @@ gcloud init
 cd pipeline/1_dataextraction
 ls
 PROJECT_ID=$(gcloud config get-value core/project)
-CONTAINER_REG_IMAGE_NAME=
+CONTAINER_REG_IMAGE_NAME=breast_cancer/1_loadingdata
+IMAGE_VERSION=v1
+IMAGE_NAME=gcr.io/$PROJECT_ID/$CONTAINER_REG_IMAGE_NAME
+
+# Build the image
+sudo docker build -t $IMAGE_NAME:$IMAGE_VERSION .
+sudo docker images
